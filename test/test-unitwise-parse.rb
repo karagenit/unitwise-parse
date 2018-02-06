923790ca55d0bb6f48444c0c6433a51a00d2a040
Caleb Smith <karagenit@outlook.com>
2018-02-06 12:33:52 -0500

    Add Tests

--- test/test-unitwise-parse.rb
+++ test/test-unitwise-parse.rb
@@ -11,12 +11,22 @@ class TestQuery < Test::Unit::TestCase
     assert_not_nil Unitwise.parse("10 m/s2")
   end
 
-  def test_fail
+  def test_parse_fail
     assert_nil Unitwise.parse("")
     assert_nil Unitwise.parse(nil)
     assert_nil Unitwise.parse("5")
     assert_nil Unitwise.parse("g")
     assert_nil Unitwise.parse("5 Not-A-Unit")
     assert_nil Unitwise.parse("5 kg s m")
+  end
+
+  def test_string
+    assert_not_nil "10 g".to_unitwise
+    assert_not_nil "1.3 g".to_unitwise
+  end
+
+  def test_string_fail
+    assert_nil "".to_unitwise
+    assert_nil "Hello".to_unitwise
   end
 end
