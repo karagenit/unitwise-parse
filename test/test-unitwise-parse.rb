00a76d49560ca738f7a177b88be845c48f1470af
Caleb Smith <karagenit@outlook.com>
2018-02-06 12:14:24 -0500

    Add Tests

--- test/test-unitwise-parse.rb
+++ test/test-unitwise-parse.rb
@@ -4,7 +4,19 @@ require_relative '../lib/unitwise/parse.rb'
 require 'test/unit'
 
 class TestQuery < Test::Unit::TestCase
-  def testExample
-    assert true
+  def test_parse
+    assert_not_nil Unitwise.parse("10 g")
+    assert_not_nil Unitwise.parse("1.3 g")
+    assert_not_nil Unitwise.parse("10 ft.lb")
+    assert_not_nil Unitwise.parse("10 m/s2")
+  end
+
+  def test_fail
+    assert_nil Unitwise.parse("")
+    # assert_nil Unitwise.parse(nil) # FIXME: no method error
+    assert_nil Unitwise.parse("5")
+    assert_nil Unitwise.parse("g")
+    assert_nil Unitwise.parse("5 Not-A-Unit")
+    assert_nil Unitwise.parse("5 kg s m")
   end
 end
